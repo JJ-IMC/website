6051e03d12c5f8ea114a8690edf978e22df44a05
HoJeong Go <seia@outlook.kr>
2022-07-26 13:12:27 +0900

    feat(mobile): navigation bar

--- sources/app/styles/layouts/navigation.tsx
+++ sources/app/styles/layouts/navigation.tsx
@@ -1,8 +1,15 @@
+import {useState} from 'react';
 import {HorizontalListItem, HorizontalListParent} from '../components/list/horizontal';
 import {styled} from '../provider';
+import MobileNavigationCollapseButtonImage from '~/../files/m-nav-col.svg';
 
 export const NavigationParent = styled(HorizontalListParent, {
 	gap: '50px',
+	margin: 0,
+
+	'@media only screen and (max-width: 1000px)': {
+		display: 'none',
+	},
 });
 
 export const NavigationItem = styled(HorizontalListItem, {
@@ -17,6 +24,63 @@ export const NavigationItem = styled(HorizontalListItem, {
 				fontWeight: '700',
 			},
 		},
+	},
+});
+
+export const MobileNavigationParent = styled(HorizontalListParent, {
+	margin: 0,
+	justifyContent: 'space-between',
+
+	backgroundColor: 'black',
+
+	'@media only screen and (min-width: 999px)': {
+		display: 'none',
+	},
+});
+
+export const MobileNavigationHeader = styled('h1', {
+	color: '$brand',
+	fontWeight: '700',
+	fontSize: '16px',
+	lineHeight: '24px',
+	margin: '0',
+});
+
+export const MobileNavigationCollapseButton = styled('img', {
+	zIndex: 2,
+	height: '20px',
+	width: '20px',
+	objectFit: 'contain',
+	cursor: 'pointer',
+});
+
+export const MobileNavigationCollapseBox = styled('ul', {
+	zIndex: 2,
+	backgroundColor: 'black',
+
+	listStyle: 'none',
+	paddingLeft: 0,
+
+	height: 0,
+	margin: 0,
+	overflow: 'hidden',
+	transition: 'height .3s ease',
+
+	variants: {
+		isActive: {
+			true: {
+				height: '100px',
+			},
+		},
+	},
+
+	'& > li': {
+		listStyle: 'none',
+		paddingLeft: 0,
+	},
+
+	'@media only screen and (min-width: 999px)': {
+		display: 'none',
 	},
 });
 
@@ -59,23 +123,53 @@ export const listItems: Array<{
 ];
 
 export function Navigation() {
+	const [mobileCollapseOpen, setMobileCollapseOpen] = useState(false);
+
+	const onCollapseButtonClick = () => {
+		setMobileCollapseOpen(!mobileCollapseOpen);
+	};
+
 	return (
-		<NavigationParent
-			css={{
-				margin: 0,
-			}}
-		>
-			{
-				listItems
-					.map(listItem => (
-						<NavigationItem
-							key={'jjimc-list-' + listItem.slug}
-							isActive={listItem.isActive}
-						>
-							{listItem.name}
-						</NavigationItem>
-					))
-			}
-		</NavigationParent>
+		<>
+			{/* Desktop section */}
+			<NavigationParent>
+				{
+					listItems
+						.map(listItem => (
+							<NavigationItem
+								key={'jjimc-list-' + listItem.slug}
+								isActive={listItem.isActive}
+							>
+								{listItem.name}
+							</NavigationItem>
+						))
+				}
+			</NavigationParent>
+			{/* Mobile section */}
+			<MobileNavigationCollapseBox isActive={mobileCollapseOpen}>
+				{
+					listItems
+						.filter(item => !item.isActive)
+						.map(listItem => (
+							<NavigationItem
+								key={'jjimc-list-' + listItem.slug}
+								isActive={listItem.isActive}
+							>
+								{listItem.name}
+							</NavigationItem>
+						))
+				}
+			</MobileNavigationCollapseBox>
+			<MobileNavigationParent>
+				<MobileNavigationHeader>
+					JJIMC
+				</MobileNavigationHeader>
+				<MobileNavigationCollapseButton
+					src={MobileNavigationCollapseButtonImage}
+					alt='button'
+					onClick={onCollapseButtonClick}
+				/>
+			</MobileNavigationParent>
+		</>
 	);
 }
